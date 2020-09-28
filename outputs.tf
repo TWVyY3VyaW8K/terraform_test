b544703b6e13c8e44b9b133886c932e4b5aecf79
Daniele Bianchin <bbianchind@gmail.com>
2020-09-28 09:06:39 +0200

    Verione outputs.tf funzionante 1

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 #Outputs definition
 
-output "load_balancer_dns" {
-  description = "The DNS name of the load balancer"
-  value       = module.alb_http.this_lb_dns_name
+output "alb_dns_name" {
+  description = "DNS name of ALB"
+  value       = aws_lb.alb_http.dns_name
 }
